0f7f338dcf94b9842648a6176b9b26c608ce7787
yagihashoo <yagihashoooooo@gmail.com>
2017-10-11 22:49:21 +0900

    Chrome操作用のパッケージをChromyからPuppeteerに変更

--- browser-js/view.js
+++ browser-js/view.js
@@ -1,41 +1,21 @@
-const Chromy = require('chromy')
+const puppeteer = require("puppeteer");
 
-// not headless
-// let chromy = new Chromy({visible:true})
-let timeout = 1000;
-let chromy = new Chromy({
-    waitTimeout: timeout,
-    gotoTimeout: timeout,
-    evaluateTImeout: timeout
-});
+let key = process.env.KEY;
+let flag = process.env.FLAG;
+let url = process.env.URL;
 
-let key = process.env.KEY
-let flag = process.env.FLAG
-let url = process.env.URL
-
-try {
-    chromy.chain()
-        .goto("http://" + key + ".knock.xss.moe/AAAAAA")
-        .setCookie({name: "flag", value: flag})
-        .goto(url)
-        .waitLoadEvent()
-        .evaluate(() => {
-            return document.querySelectorAll('*').length;
-        })
-        .result((r) => console.log(r))
-        .end()
-        .then(() => chromy.close())
-} catch (e) {
-    Chromy.cleanup()
-    process.exit(1)
-}
-
-process.on('SIGINT', async () => {
-    await Chromy.cleanup()
-    process.exit(1)
-})
-
-process.on('unhandledRejection', async () => {
-    await Chromy.cleanup()
-    process.exit(1)
-})
+(async () => {
+  const opt = {
+    headless: true,
+  };
+  const browser = await puppeteer.launch(opt);
+  const page = await browser.newPage();
+  await page.goto("http://" + key + ".knock.xss.moe/AAAAAAA", {waitUntil: 'networkidle'});
+  await page.setCookie({
+    name: "flag",
+    value: flag,
+  });
+  await page.goto(url, {waitUntil: 'networkidle'});
+  await page.waitFor(3000);
+  await browser.close();
+})();
